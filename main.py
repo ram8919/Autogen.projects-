426bb9ac8c2ffad53d991814cdcfef72072d7597
ram8919 <89965706+ram8919@users.noreply.github.com>
2024-07-03 02:28:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import autogen
+
+
+def main():
+    config_list = autogen.config_list_from_json(
+        env_or_file="OAI_CONFIG_LIST.json"
+    )
+
+    # Configure the AssistantAgent
+    assistant = autogen.AssistantAgent(
+        name="Assistant",
+        llm_config={
+            "config_list": config_list
+        }
+    )
+    user_proxy = autogen.UserProxyAgent(
+        name="user",
+        human_input_mode="NEVER",
+        code_execution_config={
+            "work_dir": "coding",
+            "use_docker": False,
+        }
+    )
+
+    user_proxy.initiate_chat(assistant, message="Plot a chart of META and TESLA stock price change.")
+
+if __name__ == "__main__":
+    main()
